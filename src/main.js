4d771bc144de97416eff5e6a6a34c6360bbd77e2
Lian Ernesto <panga.compota@gmail.com>
2026-03-26 01:53:06 -0400

    Configurar variables de producción

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import './assets/main.css'
 
+const API_URL = import.meta.env.VITE_API_URL
 const app = createApp(App)
 const pinia = createPinia()
 

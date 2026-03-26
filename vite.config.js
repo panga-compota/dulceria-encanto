a96e0eddd6a40e7d5f61128bdedcd7198eca4613
Lian Ernesto <panga.compota@gmail.com>
2026-03-26 02:01:28 -0400

    fixing /

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     tailwindcss(),
     vueDevTools(),
   ],
-  base:'dulceria-encanto',
+  base: '/dulceria-encanto/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
